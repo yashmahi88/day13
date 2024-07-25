3fac204f54f6c9fe5f150c039b2b9fdc076d8024
Yash Mahindrakar <yash.mahindrakar@einfochips.com>
2024-07-25 15:58:38 +0530

    Update App.java in production branch

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,7 +1,1 @@
-package com.example;
-
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
+System.out.println("This is hello from PRODUCTION Branch");
